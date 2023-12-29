3caa5f5df245f68efa94c563ecf05fbc9ebc7d9e
metamfetemin <ahmet.eminersoy@icloud.com>
2023-12-29 03:59:32 +0300

    update

--- org/csystem/app/App.java
+++ org/csystem/app/App.java
@@ -1,8 +1,12 @@
 package org.csystem.app;
 
+import org.csystem.app.simulation.craps.CrapsSimulationApp;
+import org.csystem.app.simulation.lotto.LottoProbabilitySimulationApp;
+
 class App {
 
-	public static void main(String[] args) {
-		System.out.println("Hello Java");
-	}
+    public static void main(String[] args) {
+		LottoProbabilitySimulationApp.run();
+
+    }
 }
